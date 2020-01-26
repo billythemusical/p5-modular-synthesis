8cddd3af67ceccaf9de752ad86cbd4e25fa29d4f
Billy Bennett <40703526+billythemusical@users.noreply.github.com>
2020-01-26 01:07:21 -0500

    Added movement and trails.
    Added the force function from Dan Shiffman's Nature of Code.

--- 15_arrays_w_visuals/bubbles.js
+++ 15_arrays_w_visuals/bubbles.js
@@ -1,23 +1,77 @@
 class Bubble {
   constructor(x, y, r, obj) {
-    this.x = x;
-    this.y = y;
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
+    this.acceleration = createVector();
+    
+    // Arbitrary damping to slow down ship
+    this.damping = 0.8;
+    this.maxSpeed = 10;
+
+    // Variable for heading!
+    this.heading = 0;
+
+    // Size of particles
     this.r = r;
-    this.col;;
+    
+    // the sound object
     this.obj = obj;
+
+    this.col = 0;
     this.amp = new p5.Amplitude();
-    this.alpha;
+    this.amp.setInput(obj);
+    this.alpha = 0;
   }
   
-  display(col) {
-    this.col = col;
-    noStroke();
-    fill(this.col, this.alpha);
-    ellipse(this.x, this.y, this.r);
+    // Newton's law: F = M * A
+  applyForce(force) {
+    let f = force.copy();
+    //f.div(mass); // ignoring mass right now
+    this.acceleration.add(f);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.velocity.mult(this.damping);
+    this.velocity.limit(this.topspeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  checkEdges() {
+    if(this.position.x > (width - this.r/2)) this.velocity.x *= -1;
+    if(this.position.x < this.r/2) this.velocity.x *= -1;
+    if(this.position.y > (height - this.r/2)) this.velocity.y *= -1;
+    if(this.position.y < this.r/2) this.velocity.y *= -1;
+  }
+  
+  // Apply a thrust force
+  boost(b) {
+    // // Offset the angle since we drew the ship vertically
+    let angle = this.velocity.heading();
+    // // Polar to cartesian for force vector!
+    let force = p5.Vector.fromAngle(angle);
+    // let force = createVector(this.velocity);
+    force.mult(b);
+    // force.mult(b);
+    this.applyForce(force);
+  }
+  
+    
+  display() {
+    // noStroke();
+    this.alpha = map(this.amp.getLevel(), 0, 0.1, 0, 255);
+    fill(random(200, 210), random(200, 210), 50, this.alpha);
+    this.r = map(this.amp.getLevel(), 0, 0.1, 10, 100);
+    ellipse(this.position.x, this.position.y, this.r);
   }
   
   listen() {
-    this.amp.setInput(this.obj);
-    this.alpha = map(this.amp.getLevel(), 0, 0.3, 0, 255);
+    // map the panning to the position
+    this.obj.pan(map(this.position.x, 0, width, -1, 1));
+    // map the amplitude of the oscillator to the acceleration of the ellipse
+    let boostVal = map(this.amp.getLevel(), 0, 0.1, 0.1, 0.3);
+    // pass that boost value as a force to be applied
+    this.boost(boostVal);
   }
 }
--- 15_arrays_w_visuals/sketch.js
+++ 15_arrays_w_visuals/sketch.js
@@ -4,54 +4,68 @@ let filts = [];
 let freqs = [];
 let envs = [];
 let bubbles = [];
-let notes = [50, 55, 57, 61, 62, 67, 69, 73, 78];
+let notes = [50, 55, 57, 61, 62, 67, 69, 73, 78, 62, 67, 61, 73];
 let num = notes.length;
 let inc = 0;
+let prevPitch;
+let rev, dly, pan;
+
 
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(800, 600);
+  background('seagreen');
+  
+  // add reverb and delay
+  rev = new p5.Reverb();
+  rev.drywet(0.5);
+  dly = new p5.Delay();
+  dly.drywet(0.5);
+  
   for (i = 0; i < num; i++) {
     oscs[i] = new p5.Oscillator('saw');
-    // must have setup(); and p5.sound
+    // must have full p5.sound library
     oscs[i].freq(midiToFreq(notes[i]));
     envs[i] = new p5.Envelope();
     oscs[i].amp(envs[i]);
-    envs[i].setADSR(0.5, 0.5, 0.15, 3);
+    envs[i].setADSR(3, 0.5, 0.15, 3);
     envs[i].setRange(0.1);
-    oscs[i].start();
     oscs[i].disconnect();
+    oscs[i].start();
     filts[i] = new p5.Filter();
     filts[i].freq(midiToFreq(notes[i]*2));
     oscs[i].connect(filts[i]);
     let w = width*0.5 + random(-width*0.4, width*0.4);
     let h = height*0.5 + random(-height*0.2, height*0.2);
-    // bubbles[i] = new Bubble(w, h, 100, envs[i]);
+    bubbles[i] = new Bubble(w, h, 15, oscs[i]);
+    rev.process(oscs[i], 5, 8);
+    dly.process(oscs[i], 0.8, 0.8);
   }
 }
 
 function draw() {
-  background(200, 100, 200);
-  let interval = map(sin(frameCount*0.01), -1, 1, 40, 100);
-  // print(interval);
+  // modulate how often we look for a new note
+  let interval = map(sin(frameCount*0.01), -1, 1, 10, 20);
   if (frameCount % int(interval) == 0) {
   // if (frameCount % 10 == 0) {
     let newPitch = random(notes);
+    // if we get the same note twice, pick a new random note
+    if(newPitch == prevPitch) newPitch = random(notes);
     let cycle = inc % notes.length;
     // print(cycle, newPitch);
     inc++;
     oscs[cycle].freq(midiToFreq(newPitch));
     envs[cycle].play();
+    prevPitch = newPitch;
   }
   
-  // for(i of bubbles) {
-  //   i.display(random(255));
-  //   i.listen(envs[i]);
-  // }
-  
+  for(i of bubbles) {
+    i.update();
+    i.checkEdges();
+    i.display();
+    i.listen();
+  }  
 }
 
 // can we choose to change the key after a certain interval or if we give some input?
-
-
-
+// can the balls bounce off one another?
